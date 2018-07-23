e0e1896363b0ce26e3a24e1bb2fbce9a72296328
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-07-23 12:10:33 +0200

    added loader animation before showing results

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
-
-// Calculate results
-function calculateResults(event) {
+document.getElementById('loan-form').addEventListener('submit', function(event) {
   // prevent default behavior of form
   event.preventDefault();
 
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+});
+
+// Calculate results
+function calculateResults() {
   // grab UI elements
   const amount = document.getElementById('amount'),
         interest = document.getElementById('interest'),
@@ -27,18 +32,25 @@ function calculateResults(event) {
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);   
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);  
+    // hide loader and show results
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById('results').style.display = 'block';
   } else {
+    // hide loader and show error
+    document.getElementById('loading').style.display = 'none';
     showError('Please check your numbers');
   }
 }
 
 // Display error message
 function showError(errorText) {
+  // show error
   const errorDiv = document.querySelector('.error-msg');
   errorDiv.textContent = errorText;
   errorDiv.classList.add('error-msg--show');
 
+  // hide error after 3s
   setTimeout(() => {
     errorDiv.classList.remove('error-msg--show');
   }, 3000);
